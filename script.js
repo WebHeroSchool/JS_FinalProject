00fdc821402c235313cf19e13f29a125395329e6
Natalya Myunster <natalyamyunster@yahoo.com>
2020-02-01 00:46:57 +0100

    choosing level red rhombus change its position

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+const allButtons = document.querySelectorAll('.menu_item');
+console.log(allButtons );
+
+const chooseLevel = (elem) => {
+    allButtons.forEach((item) => item.classList.removed("checked"));
+    elem.target.classList.add("checked");
+};
 
