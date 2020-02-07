3c154307dfcfb753faab01dededda1a2ccbe0293
Natalya Myunster <natalyamyunster@yahoo.com>
2020-02-07 19:13:14 +0100

    adopted version

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
-// Rombus position by choosen level
 const allButtons = document.querySelectorAll('.menu__item');
 console.log(allButtons );
 
+const startButton = document.getElementById("start");
+console.log( startButton);
+
+const mainScreen = document.getElementById("main-screen");
+console.log( mainScreen);
+const simpleLevel = document.getElementById("table_3");
+console.log( simpleLevel);
+
+const cards = document.querySelectorAll('.table__card');
+console.log( cards);
+
+const mainContainer = document.querySelector(".container-background");
+console.log( mainContainer);
+const table3 = document.createElement("section");
+table3.classList.add("#table3");
+
+// Rombus position by choosen level
 const chooseLevel = (elem) => {
     allButtons.forEach((item) => item.classList.remove("checked"));
     elem.target.classList.add("checked");
@@ -11,20 +27,17 @@ allButtons.forEach((item) => item.addEventListener("click", chooseLevel));
 
 
 //Start menu button by click
-const startButton = document.getElementById("start");
-const mainScreen = document.getElementById("main-screen");
-const simpleLevel = document.getElementById("table_3");
 function startGame() {
+    let currentLevel = document.querySelector(".checked").getAttribute("id");
     mainScreen.remove();
-    simpleLevel.add();
+    mainContainer.appendChild(table3);
+
 
 }
 
 startButton.addEventListener("click", startGame);
 
 //переворот карты
-const cards = document.querySelectorAll('.table__card');
-
 function flipCard() {
     this.classList.toggle('flip');
 }
